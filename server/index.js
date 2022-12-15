450a776fc8b402f625dd9576894ef43498712317
Bais <32563996+nilanbais@users.noreply.github.com>
2022-12-15 22:45:44 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const WeatherAPI = require("./server_modules/api/weather_api");
 
 const server = express();
 server.listen(3000, () => console.log("Listening at port 3000"));
@@ -7,8 +8,13 @@ server.use(express.json({ limit: '1mb'}));
 
 
 // eindpunt voor weersverwachting
-server.get('/weather_report', (request, response) => {
-    console.log(request)  // test om te kijken wat er gebeurt in de handshake
-    let data = 'Dit is een weerbericht';
+server.get('/current_weather', async(request, response) => {
+    let location = JSON.stringify(request.get('location'));
+
+    let wapi = new WeatherAPI();
+
+    let data = await wapi.get_current_weather(location);
+    
+    console.log(`sending data = ${JSON.stringify(data)}`);
     response.json(data);
 });
