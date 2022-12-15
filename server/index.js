8e0f360a36e6c5639dc1ceeb72024e1ca4c9c68d
Bais <32563996+nilanbais@users.noreply.github.com>
2022-12-15 16:15:05 +0100

    Add weather_report handshake
    Handshake doelt op de koppeling van server eindpunt en frontend functie

--- server/index.js
+++ server/index.js
@@ -4,3 +4,11 @@ const server = express();
 server.listen(3000, () => console.log("Listening at port 3000"));
 server.use(express.static('public'));
 server.use(express.json({ limit: '1mb'}));
+
+
+// eindpunt voor weersverwachting
+server.get('/weather_report', (request, response) => {
+    console.log(request)  // test om te kijken wat er gebeurt in de handshake
+    let data = 'Dit is een weerbericht';
+    response.json(data);
+});
--- server/public/putils.js
+++ server/public/putils.js
@@ -2,3 +2,18 @@ function get_time() {
     let current_time = new Date().toTimeString();
     document.getElementById('time').textContent = current_time;
 }
+
+async function get_weather_report(input_var) {
+    console.log(input_var);
+    const request_options = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'query_parameter': JSON.stringify(input_var)
+        }
+    };
+    const server_response = await fetch('/weather_report', request_options);
+    const response_json = await server_response.json();
+    console.log("server_response from putils");
+    console.log(response_json);
+};
