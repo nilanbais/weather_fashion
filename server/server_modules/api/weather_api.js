6c7edfd7a6432cdd227b9db904a6cc0a606ef750
Bais <32563996+nilanbais@users.noreply.github.com>
2022-12-15 22:45:10 +0100

    Create weather_api.js

---
+++ server/server_modules/api/weather_api.js
@@ -0,0 +1,26 @@
+
+// custom
+const APIBaseClass = require("./api_base_class")
+
+
+class WeatherAPI extends APIBaseClass {
+
+    constructor() {
+        super("WeatherAPI");
+        this.name = "WeatherAPI"
+    };
+
+    async get_current_weather(location) {
+        let api_endpoint = '/current.json'
+
+        this.header = {
+            q: location
+        };
+
+        let result = await this.get_response(api_endpoint);
+        return result;
+    };
+
+};
+
+module.exports = WeatherAPI;
