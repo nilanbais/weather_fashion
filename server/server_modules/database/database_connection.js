5a87246631f140392947893cfe867b6df3120631
Bais <32563996+nilanbais@users.noreply.github.com>
2022-12-19 22:20:23 +0100

    Update database_connection.js

--- server/server_modules/database/database_connection.js
+++ server/server_modules/database/database_connection.js
@@ -1,37 +1,43 @@
 
 const {MongoClient, ServerApiVersion} = require("mongodb");
 
-class MongoDBConnection {
+class MongoDB {
 
     #base_uri = 'mongodb+srv://<USER>:<PASSWORD>@wf1.0km9jml.mongodb.net/?retryWrites=true&w=majority;'
     #client = undefined;
-    #database = undefined;
-    #collection = undefined;
+    #database = process.env.DEFAULT_DB;
+    #collection = process.env.DEFAULT_COLLECTION;
 
     #init_db_client() {
         let USER = process.env.DB_ADMIN_NAME;
         let PASSWORD = process.env.DB_ADMIN_PASSWORD;
         let db_uri = this.#base_uri.replace('<USER>', USER).replace('<PASSWORD>', PASSWORD);
+        
         this.#client = new MongoClient(db_uri, { serverApi: ServerApiVersion.v1 });
         if (this.#client !== undefined) {
-            console.log("client defined and set");
+            this.#database = this.#client.db(this.#database);
+            this.#collection = this.#database.collection(this.#collection);
         } else {
             console.log("no connection");
-        }; 
+        };
     };
 
     async test() {
         this.#init_db_client();
         try{
             await this.#client.connect();
-            await this.#client.db("admin").command({ ping: 1 });
+            await this.#client.db("wfdb").command({ ping: 1 });
             console.log("Connected successfully to server");
+
+            console.log(`database = ${this.#database.databaseName}`)
+            console.log(`collection = ${this.#collection.collectionName}`)
+
         } finally {
             // Ensures that the client will close when you finish/error
             await this.#client.close();
             console.log("done")
-        }
+        };
     };
 };
 
-module.exports = MongoDBConnection;
+module.exports = MongoDB;
