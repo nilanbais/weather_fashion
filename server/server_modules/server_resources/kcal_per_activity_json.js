4568217b3f5448425cdb4bf64277ec9492dec093
Bais <32563996+nilanbais@users.noreply.github.com>
2023-01-16 19:04:56 +0100

    Refactor of resource documents

---
+++ server/server_modules/server_resources/kcal_per_activity_json.js
@@ -0,0 +1,8 @@
+const kcal_table = {
+    "Wandelen 5 km/h": 630,
+    "Hardlopen 11 km/h": 850,
+    "Fietsen 16 km/h": 489,
+    "Fietsen 19 km/h": 567
+};
+
+module.exports = kcal_table;
---
+++ server/server_modules/server_resources/metabolism_json.js
@@ -0,0 +1,18 @@
+const metabolism_json = {
+    "Sleeping": 40,
+    "At rest, while laying down": 45,
+    "At rest, sitting": 55,
+    "At rest, standing": 70,
+    "Walking on the level, even and solid path, without load at 2 km/h": 110,
+    "Walking on the level, even and solid path, without load at 3 km/h": 140,
+    "Walking on the level, even and solid path, without load at 4 km/h": 165,
+    "Walking on the level, even and solid path, without load at 5 km/h": 200,
+    "Walking uphill, even and solid path, without load at 5° inclination and 4 km/h": 180,
+    "Walking uphill, even and solid path, without load at 15° inclination and 3 km/h": 210,
+    "Walking uphill, even and solid path, without load at 25° inclination and 3 km/h": 300,
+    "Walking downhill, even and solid path, without load at 5° inclination and 5 km/h": 135,
+    "Walking downhill, even and solid path, without load at 5° inclination and 15 km/h": 140,
+    "Walking downhill, even and solid path, without load at 5° inclination and 25 km/h": 180,
+};
+
+module.exports = metabolism_json;
