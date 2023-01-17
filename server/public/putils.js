232660e1f7ed6777ae3abe5b6705d3af0dee92f0
Bais <32563996+nilanbais@users.noreply.github.com>
2023-01-17 15:22:45 +0100

    Delete client side cookie management

--- server/public/putils.js
+++ server/public/putils.js
@@ -1,54 +1,4 @@
 
-// cookie related functions
-function getCookie(cookie_name) {
-    let name = cookie_name + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for(let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        };
-
-        if (c.indexOf(name) == 0) {
-            let result = c.substring(name.length, c.length);
-            return result
-        };
-    }
-    return "";
-};
-
-function setCookie(cookie_name, cookie_value, expires_days) {
-    const d = new Date();
-    d.setTime(d.getTime() + (expires_days * 24 * 60 * 60 * 1000));
-    let expires = "expires=" + d.toUTCString();
-    document.cookie = cookie_name + "=" + cookie_value + ";" + expires + ";path=/";
-};
-
-function deleteCookie(cookie_name) {
-    setCookie(cookie_name, '', -1);
-};
-
-function checkCookie(cookie_name) {
-    let cookie = getCookie(cookie_name);
-    if (cookie === "" || cookie == undefined) {
-        return false;
-    }; 
-    return true
-};
-
-function check_location_weather_report_cookie(location) {
-    let cookie_name = 'weather_report';
-    if (checkCookie(cookie_name)) {
-        let weather_report_cookie = JSON.parse(getCookie(cookie_name));
-        if (weather_report_cookie.location.name === location) {
-            return true;
-        };
-        console.log("Need weather report of a different location.")
-    };
-    return false;
-};
-
 // API url related functions
 function add_params_to_url(base, params) {
     let url = base + '?';  // => /current_weather?
@@ -65,34 +15,21 @@ function add_params_to_url(base, params) {
     return url;
 };
 
-async function get_weather_report(input_location) {
-
-    let cookie_name = 'weather_report';
-    console.log(`cookie found in cookies? : ${checkCookie(cookie_name)}`);
-
-    if (check_location_weather_report_cookie(input_location)) {
-        let cookie_object = getCookie(cookie_name);
-        let weather_report = JSON.parse(cookie_object);
-        console.log(`got report from cookies`);
-        return weather_report;
-    } else {
-        const server_endpoint = '/current_weather';
-        const query_parameter = {
-            location: input_location  // input moet stad of lat,long zijn
-        };
-        // pieter.com/current_weather?location=Rotterdam&kleur=groen
-        let request_url = add_params_to_url(server_endpoint, query_parameter);
+async function get_weather_report(user_location) {
     
-        const server_response = await fetch(request_url);
-        const response_json = await server_response.json();
-    
-        // setCookie({cookie_name: new String(cookie_name), cookie_value: response_json, expires_days: 1});
-        setCookie(cookie_name, JSON.stringify(response_json), 1);
-        console.log('weather_report set as cookie');
-
-        return response_json; 
+    console.log("Hello, I'm not checking cookies, but I'm requesting new data right away. Thank you");
+    const server_endpoint = '/current_weather';
+    const query_parameter = {
+        location: user_location  // input moet stad of lat,long zijn
     };
+    // pieter.com/current_weather?location=Rotterdam&kleur=groen
+    let request_url = add_params_to_url(server_endpoint, query_parameter);
+
+    const server_response = await fetch(request_url);
+    const response_json = await server_response.json();
+    return response_json
 };
+
 
 async function test_db_connection() {
     const request_options = {
